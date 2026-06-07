7ccaf2a12d4005ec6704c0c99c18c288593a289e
nagymarten <nagy.martin19@gmail.com>
2026-06-07 18:50:06 +0200

    feat: add Vercel Speed Insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import { Provider } from "@/components/ui/provider"
 import SmoothScroll from '@/components/SmoothScroll'
 import Header from '@/components/Header'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 export default function RootLayout({
   children,
@@ -14,6 +15,7 @@ export default function RootLayout({
           <Provider>
             <Header />
             {children}
+            <SpeedInsights />
           </Provider>
         </SmoothScroll>
       </body>
